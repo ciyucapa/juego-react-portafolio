c4e6ea0f59eb0a59dcd09dcd8935fb700e9c069f
Cindy Caceres Parada <cindycaceres134@gmail.com>
2023-04-03 10:42:55 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  let x = [["1.0", "1.1", "1.2"], ["2.0", "2.1", "2.2"], ["3.0", "3.1", "3.2"]];
+  let y = [["1.0", "1.1", "1.2"], ["2.0", "2.1", "2.2"], ["3.0", "3.1", "3.2"]];
+  console.log("x", x[0][1] === y[0][1])
+  console.log("y", y)
+
+  const [jugador1, setJugador1] = useState("");
+
+  const clickCheck = () => {
+    if(jugador1 === "") {
+      setJugador1("X")
+    }
+    
+    if(jugador1 === "X") {
+      setJugador1("0")
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body>
+        <div>
+          <h1>Juego de la vieja</h1>
+          <div className='tablero'>
+            <div className='caja1'>
+              <div className='caja1a' onClick={clickCheck}>{jugador1}</div>
+              <div className='caja1a' onClick={clickCheck}>{jugador1}</div>
+              <div className='caja1a' onClick={clickCheck}>{jugador1}</div>
+            </div>
+            <div className='caja2'>
+              <div className='caja2a' onClick={clickCheck}>{jugador1}</div>
+              <div className='caja2a' onClick={clickCheck}>{jugador1}</div>
+              <div className='caja2a' onClick={clickCheck}>{jugador1}</div>
+            </div>
+            <div className='caja3'>
+              <div className='caja3a' onClick={clickCheck}>{jugador1}</div>
+              <div className='caja3a' onClick={clickCheck}>{jugador1}</div>
+              <div className='caja3a' onClick={clickCheck}>{jugador1}</div>
+            </div>
+          </div>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
